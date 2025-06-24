5ec43e7d17cc9fa17ad95b30ebf60019aef31982
Jijo Daniel <jijodaniel95@gmail.com>
2025-06-24 11:32:19 -0400

    Initial commit

--- src/main/java/com/email/emailsender/EmailsenderApplication.java
+++ src/main/java/com/email/emailsender/EmailsenderApplication.java
@@ -1,14 +1,21 @@
 package com.email.emailsender;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
-public class EmailsenderApplication {
+public class EmailsenderApplication implements CommandLineRunner {
 
+	private final CountDownLatch latch = new CountDownLatch(1);
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailsenderApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		latch.await(); // Keep the application running
+	}
 }
